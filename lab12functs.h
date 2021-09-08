0de9b00904dc01c9ca42559c869b2607b64f0503
troywein <45960380+troywein@users.noreply.github.com>
2021-09-08 11:08:14 -0600

    Add files via upload

---
+++ lab12functs.h
@@ -0,0 +1,8 @@
+/** lab12functs.h
+ * =========================================================== 
+ * Name: name, date
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
---
+++ lab12main.c
@@ -0,0 +1,13 @@
+/** lab12main.c
+ * =========================================================== 
+ * Name: name, date
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
+#include <stdio.h>
+
+int main() {
+    printf("Lab12\n");
+}
